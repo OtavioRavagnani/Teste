04a145bada527e557bf23e05e510bf46836d63a9
OtavioRavagnani <otavioravagnani01@gmail.com>
2023-11-16 21:09:09 -0300

    .

--- backend/prisma/migrations/20231026150523_create_table_users/migration.sql
+++ backend/prisma/migrations/20231026150523_create_table_users/migration.sql
@@ -39,7 +39,7 @@ CREATE TABLE "orders" (
     "id" TEXT NOT NULL,
     "table" INTEGER NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT false,
-    "draft" BOOLEAN NOT NULL DEFAULT true,
+    "draft" BOOLEAN NOT NULL DEFAULT false,
     "name" TEXT,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
