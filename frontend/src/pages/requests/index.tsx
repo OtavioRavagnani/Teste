70ee22a04ba77c62a9074df823a7d5a84a454938
OtavioRavagnani <otavioravagnani01@gmail.com>
2023-12-12 00:25:41 -0300

    .

--- frontend/src/pages/requests/index.tsx
+++ frontend/src/pages/requests/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEvent } from "react";
+import { useState, FormEvent, useEffect } from "react";
 import Head from "next/head";
 import { Header } from "../../components/Header";
 import styles from "./styles.module.scss";
@@ -7,66 +7,82 @@ import { setupAPIClient } from "../../services/api";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 import { canSSRAuth } from "../../utils/canSSRAuth";
-import { api } from "../../services/apiClient";
 
-type CategoryProps = {
-  id: string;
+//-------------------------------------------------------------
+interface Category {
+  id: string; // Substitua por propriedades reais do seu objeto Category
   name: string;
-};
+  // ... outras propriedades
+}
+
+//-------------------------------------------------------------
 
 export default function Category() {
-  const [category, setCategory] = useState<CategoryProps[] | []>([]);
-  const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+  const [numberTable, setNumberTable] = useState<number>(null);
+  const [category, setCategory] = useState<number | null>(null); // Store selected category ID
+  const [categories, setCategories] = useState<Category[]>([]); // Store list of categories
+  const router = useRouter();
 
-  const [amount, setAmount] = useState("1");
-
+  // Fetch categories on component mount
   useEffect(() => {
-    async function loadInfo() {
-      const response = await api.get("/category");
-
-      setCategory(response.data);
-      setCategorySelected(response.data[0]);
-    }
+    getCategories().then((categories) => setCategories(categories));
   }, []);
 
-  const router = useRouter();
-  //-----------------------------------------------------------------
-  async function orderRequest() {}
-  //-----------------------------------------------------------------
+  async function getCategories() {
+    const apiClient = setupAPIClient();
+    const response = await apiClient.get("/category");
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      toast.error("Erro ao carregar categorias.");
+      return [];
+    }
+  }
+
+  async function newProduct(event: FormEvent) {
+    event.preventDefault();
+
+    if (!numberTable || !category) {
+      toast.error("Preencha todos os campos.");
+      return;
+    }
+
+    // Add your product adding logic here, using numberTable and category
+    // ...
+
+    toast.success("Pedido adicionado com sucesso!");
+  }
 
   return (
     <>
       <Head>
-        <title>Nova pedido - Sujeito Pizzaria</title>
+        <title>Adicionar produto - Sujeito Pizzaria</title>
       </Head>
       <div>
         <Header />
-
         <main className={styles.container}>
-          <h1>Cadastrar novo pedido</h1>
-
-          <form className={styles.form} onSubmit={orderRequest}>
+          <h1>Adicionar produto</h1>
+          <form className={styles.form} onSubmit={newProduct}>
             <input
               type="text"
               placeholder="Digite o número da mesa"
               className={styles.input}
-              //  value={numberTable}
-              //  onChange={(e) => setNumberTable(parseFloat(e.target.value))}
+              onChange={(event) => setNumberTable(Number(event.target.value))}
             />
-
-            <select className={styles.selectProduct}>
-              <option>{categorySelected?.name}</option>
+            <select
+              className={styles.select}
+              id="category"
+              onChange={(event) => setCategory(Number(event.target.value))}
+            >
+              <option value="">Selecione a categoria</option>
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
             </select>
-
-            <input
-              type="number"
-              placeholder="Digite a quantidadade do produto"
-              className={styles.input}
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
             <button className={styles.buttonAdd} type="submit">
-              Anotar o pedido
+              Adicionar o pedido
             </button>
           </form>
         </main>
@@ -74,6 +90,8 @@ export default function Category() {
     </>
   );
 }
+
+//-------------------------------------------------------------
 
 export const getServerSideProps = canSSRAuth(async () => {
   return {
